49ea55f7da6bba25a8893bcd9159c0af7c80e7eb
Jing Liu <jingliu778@gmail.com>
2022-03-25 19:59:52 -0400

    convert original files into react format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,185 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  code_quiz,
+  disaster_factor,
+  flixandSpill,
+  images,
+  img_1838,
+  screenshot_1,
+  screenshot_2,
+  screenshot_3,
+} from "./images";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <h1>Jing Liu</h1>
+          <ul class="text-right">
+            <li id="margin">
+              <a href="#about-me">About Me</a>
+            </li>
+            <li>
+              <a href="#work">Work</a>
+            </li>
+            <li>
+              <a href="#contact-me">Contact Me</a>
+            </li>
+            <li>
+              <a href="./assets/Jing Liu's Resume.pdf" download>
+                Resume
+              </a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <div
+        id="backgroud"
+        style={{
+          backgroundImage: `url(${images})`,
+        }}
+      >
+        <span
+          class="profileImage"
+          style={{
+            backgroundImage: `url(${img_1838})`,
+          }}
+        />
+      </div>
+      <main>
+        <section className="section-container">
+          <h2 id="about-me" className="sectionTitle">
+            About Me
+          </h2>
+          <div className="content-wrapper">
+            <p>
+              I am a highly-motivated, diligent and reliable individual with a
+              solid academic background in Electrical & Computer Engineering.
+              Solutions oriented and competent technical professional in
+              electronic design, system testing, and software testing.
+              <br />
+              <br />
+              Familiar with IEEE/IEC Standards and Nuclear Regulatory Commission
+              Regulatory Guidelines. Proficient in programming and Microsoft
+              Office applications. Excellent communication, interpersonal and
+              organizational skills. Strong sense of responsibility and team
+              spirit.
+            </p>
+          </div>
+        </section>
+
+        <section className="section-container">
+          <h2 id="work" className="sectionTitle">
+            Work
+          </h2>
+          <div className="content-wrapper flex-column">
+            <div className="link-images-wrapper-md">
+              <div className="container">
+                <a
+                  href="https://jing-liu-778.github.io/Disaster-Factor/"
+                  className="main-image-link"
+                >
+                  <img
+                    id="disaster-factor"
+                    className="projectImage1"
+                    src={disaster_factor}
+                    alt="disaster-factor project"
+                  />
+                  <div className="text-block">
+                    <h4>Disater Factor</h4>
+                    <p>JavaScript/CSS/HTML</p>
+                    <p className="insidelink">
+                      https://github.com/jing-liu-778/Disaster-Factor
+                    </p>
+                  </div>
+                </a>
+              </div>
+
+              <div className="container">
+                <a
+                  href="https://shielded-gorge-65326.herokuapp.com/"
+                  className="main-image-link"
+                >
+                  <img
+                    id="flixandspill"
+                    className="projectImage1"
+                    src={flixandSpill}
+                    alt="flixandspill project"
+                  />
+                  <div className="text-block">
+                    <h4>Flix and Spill</h4>
+                    <p>JavaScript/Node.js/CSS/HTML</p>
+                    <p className="insidelink">
+                      https://github.com/jing-liu-778/Flix-and-Spill
+                    </p>
+                  </div>
+                </a>
+              </div>
+            </div>
+
+            <div className="link-images-wrapper-md">
+              <div className="container">
+                <a href="https://mimitale.com/login" className="container">
+                  <img
+                    id="mimitale"
+                    className="linkImage"
+                    src={screenshot_2}
+                    alt="Mimitale project"
+                  />
+                  <div className="text-block">
+                    <h4>Mimitale</h4>
+                    <p>React/JavaScript/CSS</p>
+                  </div>
+                </a>
+              </div>
+              <div className="container">
+                <a href="https://jing-liu-778.github.io/Code-Quiz/">
+                  <img
+                    id="project3"
+                    className="linkImage"
+                    src={code_quiz}
+                    alt="code-quiz project"
+                  />
+                </a>
+                <a
+                  href="https://github.com/jing-liu-778/Code-Quiz"
+                  className="link-color"
+                >
+                  <div className="text-block">
+                    <h4>Code Quiz</h4>
+                    <p>JavaScript/CSS/HTML</p>
+                    <p className="insidelink">
+                      https://github.com/jing-liu-778/Code-Quiz
+                    </p>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="section-container footer-space">
+          <h2 id="contact-me" className="sectionTitle">
+            Contact Me
+          </h2>
+          <div className="content-wrapper">
+            <ul className="contact-links-wrapper">
+              <li className="phone">Phone: 201.888.2635</li>
+              <li>
+                E-Mail:
+                <a href="mailto:jingliu778@gmail.com">jingliu778@gmail.com</a>
+              </li>
+              <li>
+                <a href="https://github.com/jing-liu-778">Github</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/jing-liu-eit/">Linkedin</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/images/index.js
@@ -0,0 +1,19 @@
+import code_quiz from "./Code_Quiz.png";
+import disaster_factor from "./Disaster_Factor.png";
+import flixandSpill from "./FlixandSpill.png";
+import images from "./images.jpg";
+import img_1838 from "./IMG_1838.JPG";
+import screenshot_1 from "./screenshot-1.png";
+import screenshot_2 from "./screenshot-2.png";
+import screenshot_3 from "./screenshot-3.png";
+
+export {
+  code_quiz,
+  disaster_factor,
+  flixandSpill,
+  images,
+  img_1838,
+  screenshot_1,
+  screenshot_2,
+  screenshot_3,
+};
